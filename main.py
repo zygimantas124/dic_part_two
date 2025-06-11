80f2fa1383564bb5b05ebef0a73037aef702f0b7
Dazai <c.c.e.lee@student.tue.nl>
2025-06-11 21:37:01 +0200

    adjust to new training files

--- main.py
+++ main.py
@@ -2,21 +2,17 @@ import sys
 import logging
 
 from util.helpers import parse_args, setup_logger
-from util.training import train_ppo, train_dqn
+from util.training import train  
 
-
-# ---------- Entry Point ----------
 if __name__ == "__main__":
     args = parse_args()
     logger = setup_logger(log_level=logging.DEBUG)
 
-    logger.info(f"Training started with algorithm: {args.algo}")
+    logger.info(f"Training started with algorithm: {args.algo.upper()}")
     logger.info(f"Args: {args}")
 
-    if args.algo == "ppo":
-        train_ppo(args, logger)
-    elif args.algo == "dqn":
-        train_dqn(args, logger)
+    if args.algo in ["ppo", "dqn"]:
+        train(args, logger)
     else:
         logger.error(f"Unknown algorithm specified: {args.algo}")
         sys.exit(1)
