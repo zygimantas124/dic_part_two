96e3b573624134de38f81a4986e7276a6c760f63
Dazai <c.c.e.lee@student.tue.nl>
2025-06-13 15:11:20 +0200

    clear out logs before starting new run

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
 import logging
+import shutil
+import os
+
 
 from util.helpers import parse_args, setup_logger
 from util.training import train  
 
 if __name__ == "__main__":
+    log_dir = "./logs"
+    if os.path.exists(log_dir):
+        shutil.rmtree(log_dir)
+        
     args = parse_args()
     logger = setup_logger(log_level=logging.DEBUG)
 
